7db8d3ca8d322255c23466dc6d5acc35722ac11a
hamletguy <ahmedayachi1607@gmail.com>
2022-12-29 11:15:13 +0100

    fixed css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -28,12 +28,24 @@ class Profile extends Component {
     const { person, timer } = this.state;
     const { fullname, bio, imgsrc, profession } = person;
     return (
-      <div>
-        <img src={imgsrc} alt="img" style={{ borderRadius: "50%" }} />
-        <div>{fullname}</div>
-        <div>{bio}</div>
-        <div>{profession}</div>
-        <div>{timer}</div>
+      <div
+        style={{
+          backgroundColor: "grey",
+          marginLeft: "400px",
+          marginRight: "400px",
+          height: "500px",
+          borderRadius: "10%",
+        }}
+      >
+        <img
+          src={imgsrc}
+          alt="img"
+          style={{ borderRadius: "50%", width: "40%" }}
+        />
+        <h1>{fullname}</h1>
+        <h3>{bio}</h3>
+        <h3>{profession}</h3>
+        <h3>{timer}</h3>
       </div>
     );
   }
